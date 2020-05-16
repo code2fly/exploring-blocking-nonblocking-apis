8faa109e9b7f6f883b4184f52e6c96133ec9f705
Mohit Pant <MOHITPAN@amdocs.com>
2020-05-16 21:46:51 +0300

    tried with future almost gave the performance as async

--- src/main/java/com/mparch/learning/reactiveprogramming/bankservice/MortgageController.java
+++ src/main/java/com/mparch/learning/reactiveprogramming/bankservice/MortgageController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 @RestController
 @Slf4j
 public class MortgageController {
@@ -15,22 +20,28 @@ public class MortgageController {
 
     private final CreditCheckReportController creditCheckService;
 
+    private ExecutorService executorService = Executors.newFixedThreadPool(20);
+
     public MortgageController(AccountReportController accountService, CreditCheckReportController creditCheckService) {
         this.accountService = accountService;
         this.creditCheckService = creditCheckService;
     }
 
     @GetMapping("/mortgage")
-    public MortgageReport getMortgageReport(@RequestParam("custId") String customerId) {
+    public MortgageReport getMortgageReport(@RequestParam("custId") String customerId) throws ExecutionException, InterruptedException {
         log.info("First thread while receiving request is :  {} ", Thread.currentThread());
 
         String accountDataForCustomer = accountService.getAccountDataForCustomer(customerId);
+//        Future<String> accountDataFuture = executorService.submit(() -> accountService.getAccountDataForCustomer(customerId));
         log.info("Second thread after calling ACCOUNT service receiving request is :  {} ", Thread.currentThread());
         String creditReportForCustomer = creditCheckService.getCreditReportForCustomer(customerId);
+//        Future<String> creditReportFuture = executorService.submit(() -> creditCheckService.getCreditReportForCustomer(customerId));
         log.info("Third thread after calling CREDIT CHECK service receiving request is :  {} ", Thread.currentThread());
 
         return new MortgageReport(accountDataForCustomer, creditReportForCustomer);
+//        return  new MortgageReport(accountDataFuture.get(), creditReportFuture.get());
     }
+
 }
 
 
