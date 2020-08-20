694ceeb8b1ec2c42790c72d019d994d7f00bf430
Mohit Pant <MOHITPAN@amdocs.com>
2020-08-20 21:31:55 +0300

    added comments for best serialization format

--- src/main/java/com/mparch/learning/reactiveprogramming/ReactiveProgrammingApplication.java
+++ src/main/java/com/mparch/learning/reactiveprogramming/ReactiveProgrammingApplication.java
@@ -21,12 +21,19 @@ public class ReactiveProgrammingApplication {
 	}
 
 
+	/*
+	Best serialization format -
+	If you are looking for a battle-tested, strongly typed serialization format, then Protocol Buffers is a great choice.
+	 If you also need a variety of built-in RPC mechanisms, then Thrift is worth investigating.
+	 If you are already exchanging or working with JSON, then MessagePack is almost a drop-in optimization.
+	  And finally, if you like the strongly typed aspects, but want the flexibility of easy interoperability with dynamic languages, then Avro may be your best bet at this point in time
+	 */
 	@Configuration
 	static class WebConfig implements WebMvcConfigurer {
 
 		HttpMessageConverter messagePackMessageConverter() {
-			MediaType msgpackMediaType = new MediaType("application", "x-msgpack");
-			return new AbstractJackson2HttpMessageConverter(new ObjectMapper(new MessagePackFactory()), msgpackMediaType) {
+			MediaType msgPackMediaType = new MediaType("application", "x-msgpack");
+			return new AbstractJackson2HttpMessageConverter(new ObjectMapper(new MessagePackFactory()), msgPackMediaType) {
 			};
 		}
 
