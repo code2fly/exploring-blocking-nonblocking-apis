bbd26e757e5dd239420b33f0122c4a13755a77a1
Mohit Pant <MOHITPAN@amdocs.com>
2020-08-20 20:36:31 +0300

    added msgpack usage for spring to serialize msg to bytes and reduce payload to increase performance

--- src/main/java/com/mparch/learning/reactiveprogramming/ReactiveProgrammingApplication.java
+++ src/main/java/com/mparch/learning/reactiveprogramming/ReactiveProgrammingApplication.java
@@ -1,13 +1,39 @@
 package com.mparch.learning.reactiveprogramming;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.msgpack.jackson.dataformat.MessagePackFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @SpringBootApplication
 public class ReactiveProgrammingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveProgrammingApplication.class, args);
+	}
+
+
+	@Configuration
+	static class WebConfig implements WebMvcConfigurer {
+
+		HttpMessageConverter messagePackMessageConverter() {
+			MediaType msgpackMediaType = new MediaType("application", "x-msgpack");
+			return new AbstractJackson2HttpMessageConverter(new ObjectMapper(new MessagePackFactory()), msgpackMediaType) {
+			};
+		}
+
+		@Override
+		public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+			 converters.add(messagePackMessageConverter());
+		}
 	}
 
 }
